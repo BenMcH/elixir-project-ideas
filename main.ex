073e3e9e064882ebb4bc7ad02a8e4a23aa832174
Ben McHone <ben@mchone.dev>
2022-05-30 20:57:44 -0500

    Basic elixir project generator

---
+++ main.ex
@@ -0,0 +1,25 @@
+{:ok, contents} = File.read('./products.txt')
+products = contents |> String.split("\n", trim: true)
+
+{:ok, contents} = File.read('./dempgraphics.txt')
+dempgraphics = contents |> String.split("\n", trim: true)
+
+number = case IO.gets("How many product suggestions would you like?\n") |> String.trim |> Integer.parse do
+  {x, ""} -> x
+  _ -> exit('Invalid number')
+end
+
+defmodule Loop do
+   def print_suggestion(func, n) when n <= 1 do
+      func.()
+   end
+
+   def print_suggestion(func, n) do
+      func.()
+      print_suggestion(func, n - 1)
+   end
+end
+
+product_suggestion = fn -> IO.puts(Enum.random(products) <> " for " <> Enum.random(dempgraphics)) end
+
+Loop.print_suggestion(product_suggestion, number)
